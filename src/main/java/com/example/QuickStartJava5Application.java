a768612a9ed2d0994f1df44aef4b84d178481b8d
장재훈 <codelife@11st.co.kr>
2016-10-03 21:09:39 +0900

    QuickStart with Spring Boot

---
+++ src/main/java/com/example/QuickStartJava5Application.java
@@ -0,0 +1,30 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+    @Configuration
+    @EnableAutoConfiguration
+    @ComponentScan
+    @SpringBootApplication
+public class QuickStartJava5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickStartJava5Application.class, args);
+		 System.out.println(" QuickStartJava5Application Spring Boot Started.");
+	}
+	
+//	@Bean
+//    public InternalResourceViewResolver setupViewResolver() {
+//        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+//        resolver.setPrefix("/WEB-INF/view/");
+//        resolver.setSuffix(".jsp");
+//        return resolver;
+//    }
+	
+}
