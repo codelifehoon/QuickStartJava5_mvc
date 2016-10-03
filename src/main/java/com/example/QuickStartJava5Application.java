e9dbea67b8bf84e064c612539418dc79e0c941c4
장재훈 <codelife@11st.co.kr>
2016-10-03 22:33:48 +0900

    11

--- src/main/java/com/example/QuickStartJava5Application.java
+++ src/main/java/com/example/QuickStartJava5Application.java
@@ -3,10 +3,8 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
     @Configuration
     @EnableAutoConfiguration
